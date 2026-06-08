7682fd63586700d16ba9e3f82631ca40274c5ecb
tarshar4242 <tarshar@ms9.hinet.net>
2026-06-08 17:29:14 +0800

    Add service worker

---
+++ sw.js
@@ -0,0 +1,28 @@
+const cacheName = "lingua-buddy-v7";
+const assets = [
+  "./",
+  "./index.html",
+  "./styles.css?v=5",
+  "./app.js?v=6",
+  "./manifest.webmanifest?v=5",
+  "./supabase-config.js?v=2",
+  "./setup.html",
+  "./icon.svg",
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(caches.open(cacheName).then((cache) => cache.addAll(assets)));
+});
+
+self.addEventListener("activate", (event) => {
+  event.waitUntil(
+    caches
+      .keys()
+      .then((keys) => Promise.all(keys.filter((key) => key !== cacheName).map((key) => caches.delete(key))))
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  if (event.request.method !== "GET") return;
+  event.respondWith(caches.match(event.request).then((cached) => cached || fetch(event.request)));
+});
