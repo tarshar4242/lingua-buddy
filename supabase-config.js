dd362ad009882b8cc66c6c98890d3efff50ed78c
tarshar4242 <tarshar@ms9.hinet.net>
2026-06-08 17:28:37 +0800

    Add Supabase config

---
+++ supabase-config.js
@@ -0,0 +1,6 @@
+window.LINGUA_BUDDY_SUPABASE = {
+  enabled: true,
+  url: "https://vmxhfkmhvscylfheovtr.supabase.co",
+  anonKey: "sb_publishable_orasiC3vP7epYzs7xOKjpQ__NueDelR",
+  ownerId: "my-phone-learning",
+};
